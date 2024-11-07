9b612ab1ec1ac3e4bd1fa23e4ad6aad45278c22d
uploveks <daniela.belibov@stud.acs.upb.ro>
2024-11-07 16:55:55 +0200

    done B

--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_2/Colocviu1_2MainActivity.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_2/Colocviu1_2MainActivity.kt
@@ -1,10 +1,13 @@
 package ro.pub.cs.systems.eim.Colocviu1_2
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -31,6 +34,22 @@ class Colocviu1_2MainActivity : AppCompatActivity() {
             } else {
                 Log.d("Concatenare", "Concatenarea este ignorata, camp null")
             }
+        }
+
+        val activityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val resultIntent = result.data
+                val sum = resultIntent?.getIntExtra("sum", 0)
+                Toast.makeText(this, "The sum is $sum", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        val navigateSecondActivity = findViewById<Button>(R.id.compute_button)
+        navigateSecondActivity.setOnClickListener {
+            val terms = allTerms.text.toString().trimEnd('+')
+            val intent = Intent(this, Colocviu1_2SecondaryActivity::class.java)
+            intent.putExtra("addTerms", terms)
+            activityResult.launch(intent)
         }
 
     }
--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_2/Colocviu1_2SecondaryActivity.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_2/Colocviu1_2SecondaryActivity.kt
@@ -1,10 +1,31 @@
 package ro.pub.cs.systems.eim.Colocviu1_2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class Colocviu1_2SecondaryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val addTerms = intent.getStringExtra("addTerms")
+
+        var sum = 0
+        if (addTerms.isNullOrEmpty()) {
+            setResult(RESULT_CANCELED)
+            finish()
+            return
+        } else {
+            val addTermsList = addTerms.split("+")
+            sum = addTermsList.sumOf { it.toInt() }
+        }
+
+        setResult(RESULT_OK, Intent().apply {
+            putExtra("sum", sum)
+        })
+
+        finish()
+
+
     }
 }
